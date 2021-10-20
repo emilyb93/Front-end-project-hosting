547f7d6e639779eda2915c61415ca84890eb57c6
emilyb93 <emilybennett93@hotmail.com>
2021-10-20 15:55:40 +0100

    added account link image in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  const [enableMenu, setEnableMenu] = useState(false);
   return (
     <header>
       <h1 className="header-text">NC NEWS</h1>
+      <Link to="/account">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          width="50rem"
+          alt="account-link"
+        />
+      </Link>
       <button
         className="nav-button"
         onClick={(e) => {
           e.preventDefault();
+          setEnableMenu(true);
         }}
       >
         =
       </button>
+      {enableMenu && (
+        <ul className="menu">
+          <li id="menu-close">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setEnableMenu(false);
+              }}
+            >
+              X
+            </button>
+          </li>
+          <li>
+            <Link to="/articles">Articles</Link>
+          </li>
+          <li>
+            <Link to="/account">Account</Link>
+          </li>
+        </ul>
+      )}
     </header>
   );
 };
